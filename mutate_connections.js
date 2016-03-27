983e93739ed1e81b0e6274265d6692838b86ff20
Sean W. Mahan <smahan@knowledge-architecture.com>
2016-03-26 22:12:50 -0700

    More mutate_connections improvements.

--- mutate_connections.js
+++ mutate_connections.js
@@ -1,6 +1,7 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('adventureConnections.sqlite');
 var nlp = require("nlp_compromise");
+var RiTa = require('rita');
 var fs = require('fs');
 
 var places = ['Aelley', 'Wildesnow', 'Iceriver', 'Whiteash', 'Violetwitch', 'Linport', 'Lightmaple', 'Redwheat', 'Wintercourt', 'Clearmoor', 'Strongshore', 'Griffinview', 'Greyview', 'Ashbridge', 'Silverwitch', 'Byway', 'Pinefort', 'Landhall', 'Ericliff', 'Woodlake', 'Wintercliff', 'Beachlea', 'Freyhill', 'Beechley', 'Snowrock', 'Faywynne', 'Snowden', 'Summerston', 'Swynham', 'Faycrest', 'Brightbell', 'Brookdale', 'Rayacre', 'Coldmont', 'Roseden', 'Bluelea', 'Esterflower', 'Mortown', 'Elfton', 'Greybay', 'Baymeadow', 'Delledge', 'Silveredge', 'Waymill', 'Violethall', 'Northmage', 'Crystalmist', 'Meadowmoor', 'Bayston', 'Butterbourne', 'Oldloch', 'Spellmarsh', 'Greyhollow', 'Crystalwyn', 'Waterhollow', 'Lincliff', 'Blackbridge', 'Faysnow', 'Dorden'];
@@ -99,6 +100,9 @@ function replaceAfter(haystack, needle, pin, minIndex)
 {
 	if (verbose) console.log("Needle start: " + needle);
 
+	if (needle.toUpperCase()==needle && pin!=null)
+		pin = pin.toUpperCase();
+
 	super_original_needle = needle;
 
 	var loop = true;
@@ -134,21 +138,12 @@ function parseConnection(text, connectionID, callback)
 	var descriptions = [];
 
 	text = text.trim();
-	text = text.replace(/so i know it\'s you/gi," ");
-	text = text.replace(/(I)\'ve/gi,"$1 has");
-	text = text.replace(/(you)\'ve/gi,"$1 have");
-	text = text.replace(/(I)\'m/gi,"$1 is");
 	//text = text.replace(/(I)\'d/gi,"$1");
 	//text = text.replace(/(You)\'d/gi,"$1");
-	text = text.replace(/(you)\'re/gi,"$1 are");
-	text = text.replace(/[Iu]\'ll/ig, " will");
 	//text = text.replace(/(couldn\'t)/gi,"can't");
 	//text = text.replace(/(could)/gi,"can");
 	//text = text.replace(/(you wouldn\'t)/gi,"you don't");
-//	text = text.replace(/(wouldn\'t)/gi,"doesn't");
-	text = text.replace(/(wasn\'t)/gi,"isn't");
-	text = text.replace(/I have/g,"I has");
-	text = text.replace(/((Monday|Tuesday|Wednesday|Thursday|Friday|today|tonight|yesterday)[\.\,\?\!])/g," ");
+	//text = text.replace(/(wouldn\'t)/gi,"doesn't");
 	var parsed = nlp.pos(text);
 
 	for (var senti = parsed.sentences.length - 1; senti >= 0; senti--) {
@@ -160,6 +155,8 @@ function parseConnection(text, connectionID, callback)
 		if (sentence.verbs().length==0)
 			continue;
 
+		var ALL_CAPS_MODE = (sentence.text()==sentence.text().toUpperCase());
+
 		var sentenceText = sentence.text().trim();
 
 		if (sentenceText.length>110)
@@ -168,9 +165,18 @@ function parseConnection(text, connectionID, callback)
 		if (sentenceText.charAt(sentenceText.length -1)=='?')
 			continue;
 
+		sentenceText = sentenceText.replace(/(you)\'re/gi,"$1 are");
+		sentenceText = sentenceText.replace(/[Iu]\'ll/ig, " will");
+		sentenceText = sentenceText.replace(/so i know it\'s you/gi," ");
+		sentenceText = sentenceText.replace(/(I)\'ve/gi,"$1 has");
+		sentenceText = sentenceText.replace(/(you)\'ve/gi,"$1 have");
+		sentenceText = sentenceText.replace(/(I)\'m/gi,"$1 is");
+		sentenceText = sentenceText.replace(/(wasn\'t)/gi,"isn't");
+		sentenceText = sentenceText.replace(/I have/gi,"I has");
+		sentenceText = sentenceText.replace(/((Monday|Tuesday|Wednesday|Thursday|Friday|today|tonight|yesterday)[\.\,\?\!])/gi," ");
 		sentenceText = sentenceText.replace(/^["'* \.-]*(Yesterday|Last night|Today|Tonight|Monday|tuesday|wednesday|thursday|Friday|Saturday|Sunday|this afternoon|this morning|this evening)+[\s](night|evening|afternoon|morning)*/," ");
-		sentenceText = sentenceText.replace(/(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|January|February|April|June|July|August|September|October|November|December|(at )*about|around|night|(this )*morning|(this )*evening|(this )*afternoon|today|between|(the [0-9]+(th|nd|st)*)|([0-9]+[\s]*(th|nd|st))|([0-9]+,[\s]*(19|20)[0-9]{2})|([0-9]+[\s]*[pam]{2})|((and )*[0-9]{1,2}[: ]+[0-9]{2}[\s]*(pm|am)*)|[\s,]){4,}/g," ");
-		sentenceText = sentenceText.replace(/([0-9]{1,4}\/[0-9]{1,2}\/[0-9]{1,4})/g," ");
+		sentenceText = sentenceText.replace(/(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|January|February|April|June|July|August|September|October|November|December|(at )*about|around|night|(this )*morning|(this )*evening|(this )*afternoon|today|between|(the [0-9]+(th|nd|st)*)|([0-9]+[\s]*(th|nd|st))|([0-9]+,[\s]*(19|20)[0-9]{2})|([0-9]+[\s]*[pam]{2})|((and )*[0-9]{1,2}[: ]+[0-9]{2}[\s]*(pm|am)*)|[\s,]){4,}/gi," ");
+		sentenceText = sentenceText.replace(/([0-9]{1,4}\/[0-9]{1,2}\/[0-9]{1,4})/gi," ");
 
 		if (/missed connections/gi.test(sentenceText)
 			||
@@ -178,7 +184,7 @@ function parseConnection(text, connectionID, callback)
 			||
 			/(reply|respond)/gi.test(sentenceText)
 			||
-			/(pussy|fuck|shit|slut|bitch|nigger|fag)/gi.test(sentenceText)
+			/(pussy| tits|fuck|shit|slut|bitch|nigger|fag)/gi.test(sentenceText)
 			||
 			/jack(ing|ed) off/gi.test(sentenceText)
 			||
@@ -275,7 +281,7 @@ function parseConnection(text, connectionID, callback)
 		var last_verb_you = false;
 
 		var removeIfPastTenseAfter = ["has","have","should've","shouldve"];
-		var useInfAfter = ["could","couldnt","should","shouldn't","would","wouldn't","had","to","does","do"];
+		var useInfAfter = ["could","couldnt","should","shouldn't","shouldnt","would","wouldn't","wouldnt","had","to","does","do","hasn't","hasnt"];
 		var last_subject = "";
 		var token_index = 0;
 
@@ -285,8 +291,13 @@ function parseConnection(text, connectionID, callback)
 		mangled = sentence.text();
 		if (verbose) console.log("Pre token-loop: " + mangled)
 
+		ritatags = RiTa.RiTa.getPosTags(mangled);
+
 		for (var tokeni = 0; tokeni < sentence.tokens.length; tokeni++) 
 		{
+			token_tag = ritatags[tokeni].toUpperCase();
+			next_token_tag = ritatags[tokeni + 1];
+
 			token = sentence.tokens[tokeni];
 			if (token.text==" " || token.text=="")
 				continue;
@@ -296,12 +307,13 @@ function parseConnection(text, connectionID, callback)
 			if (sentence.tokens.length > tokeni + 2 && (next_token.text==" " || next_token.text==""))
 			{
 				next_token = sentence.tokens[tokeni + 2];
+				next_token_tag = ritatags[tokeni + 2];
 			}
 
 			token_index = mangled.indexOf(token.text, token_index);
-			if (verbose) console.log("Token: " + token.text + " (" + token_index + "," + token.pos.tag + ")");
+			if (verbose) console.log("Token: " + token.text + " (" + token_index + "," + token.pos.tag + "/" + token_tag +")");
 
-			if (sentence.tokens.length > tokeni + 1 && next_token.pos.tag=="VBD" && removeIfPastTenseAfter.indexOf(token.text)>-1)
+			if (sentence.tokens.length > tokeni + 1 && next_token_tag=="VBD" && removeIfPastTenseAfter.indexOf(token.text.toLowerCase())>-1)
 			{
 				if (verbose) console.log("Removing previous token. Last token: " + last_token);
 				mangled = replaceAfter(mangled, token.text, "", token_index);
@@ -321,7 +333,7 @@ function parseConnection(text, connectionID, callback)
 				last_subject = "we";
 				if (verbose) console.log("last subject: we (" + token_text + ")");
 			}
-			else if (last_token_pos!="IN"  && (token_text.toLowerCase()=="i" || token_text.toLowerCase()=="it" || token.pos.tag=="NN"))
+			else if (last_token_pos!="IN"  && (token_text.toLowerCase()=="i" || token_text.toLowerCase()=="it" || token_tag=="NN"))
 			{
 				last_subject = "it";
 				if (verbose) console.log("last subject: I/it (" + token_text + ")");
@@ -330,10 +342,10 @@ function parseConnection(text, connectionID, callback)
 			/*
 			* First some manual things.
 			*/
-			if (token.pos.tag=="VBD" || token.pos.tag=="CP" || token.pos.tag=="VB" || token.pos.tag=="VBP" || token.pos.tag=="VBZ" || token.pos.tag=="CP")
+			if (token_tag=="VBD" || token_tag=="CP" || token_tag=="VB" || token_tag=="VBP" || token_tag=="VBZ" || token_tag=="CP")
 			{
 				if (verbose) console.log("last_token: "+last_token);
-				if (verbose) console.log("Verb: "+token_text + " (" + token.pos.tag + ")");
+				if (verbose) console.log("Verb: "+token_text + " (" + token.pos.tag + "/" + token_tag + ")");
 				var vbtext, vbpres, vbinf = "";
 				try{
 					vbtext = token.text;
@@ -356,20 +368,26 @@ function parseConnection(text, connectionID, callback)
 					vbinf  = nlp.verb(vbtext).conjugate().infinitive;
 				}
 
-				if (useInfAfter.indexOf(last_token)==-1  &&
-					(last_subject=="it" || token.pos.tag=="CP") )
+				if (useInfAfter.indexOf(last_token.toLowerCase())==-1  &&
+					(last_subject=="it" || token_tag=="CP") )
 				{
 					mangled = replaceAfter(mangled, token.text, vbpres, token_index);				
 					if (verbose) console.log("pres swap: " + vbpres);
 				}
-				else if (last_subject=="you" || last_subject=="we" || useInfAfter.indexOf(last_token)>-1)
+				else if (last_subject=="you" || last_subject=="we" || useInfAfter.indexOf(last_token.toLowerCase())>-1)
 				{
 					mangled = replaceAfter(mangled, token.text, vbinf, token_index);
 					if (verbose) console.log("inf swap: " + vbinf);
 				}
-			}
+			}/*
+			else if (useInfAfter.indexOf(last_token)>-1  &&
+                                        (last_subject=="it" || token_tag=="CP") )
+                                {
+                                        mangled = replaceAfter(mangled, token.text, vbpres, token_index);
+                                        if (verbose) console.log("pres swap: " + vbpres);
+                                }*/
 
-			last_token_pos = token.pos.tag;
+			last_token_pos = token_tag;
 
 			last_token = token_text;
 		};
@@ -468,6 +486,9 @@ function parseConnection(text, connectionID, callback)
 			mangled = mangled.slice(0,mangled.length-1);
 
 		mangled = prettify(mangled);
+
+		if (ALL_CAPS_MODE)
+			mangled = mangled.toUpperCase();
 
 		if (/^You[\s,]/.test(mangled))
 		{
